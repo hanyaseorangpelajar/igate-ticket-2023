539c511b6a684e8ca6f550962794a64fc7ec1bc7
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-10-31 06:14:49 +0700

    intial-commit

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'payment', label: 'Payment' },
+    { href: '/', key: 'form', label: 'Form' },
+  ];
