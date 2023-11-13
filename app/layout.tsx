03dc5046782f9d7313f167dcf87c0437f022134a
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-11-13 14:26:56 +0700

    fix: header width

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export const metadata: Metadata = {
-  title: 'I-Tix | Welcome',
-  description: 'Ticketing system for I-Gate 2023',
-}
+  title: "I-Tix | Welcome",
+  description: "Ticketing system for I-Gate 2023",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='overflow-auto bg-zinc-800 text-white border border-white border-solid border-4 m-2 font-mono'>
+      <body className="overflow-auto bg-zinc-800 text-white border border-white border-solid border-4 m-2 font-mono">
         <Header />
-        <main className='relative'>
-          {children}
-        </main>        
+        <main className="relative">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <nav className="bg-black flex justify-center items-center max-container padding-container relative z-30 py-5">
+    <nav className="bg-black flex justify-center items-center padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/logo.png" alt="logo" width={74} height={29} />
       </Link>
